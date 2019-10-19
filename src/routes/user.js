3703679648a8612a6d7e92a6ef172e3bc0025df4
reshinto <terencekong2002@gmail.com>
2019-10-20 05:58:38 +0800

    Modified files for server & client connection test

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,15 @@ import {Router as route} from "express";
 
 const router = route();
 
+console.log(process.env.SALT)
 /**
  * GET home page
  */
 router.get('/', (req, res) => {
-  res.send("home")
+  const data = {
+    userData: "from the server"
+  }
+  res.send(data)
 });
 
 export default router;
