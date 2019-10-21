95666703bd845a3e2baea746754fa3bbb18ebbc9
reshinto <terencekong2002@gmail.com>
2019-10-22 03:33:45 +0800

    Add middleware functions

--- src/handlers/utils.js
+++ src/handlers/utils.js
@@ -1,3 +1,7 @@
+import {sha256} from "js-sha256";
+
+const SALT = process.env.SALT;
+
 function checkBlanks(req) {
   const keys = Object.keys(req.body)
   for (let i=0; i<keys.length; i++) {
@@ -11,4 +15,31 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-export {checkBlanks, validateEmail};
+async function validation(pool, req, res) {
+  const getUserId = req.cookies["token"];
+  let queryText = "SELECT user_id FROM songs";
+  const result = await pool.query(queryText)
+  return verifyUser(getUserId, result);
+}
+
+
+function verifyUser(hashedId, result) {
+  for (let i=0; i<result.rows.length; i++) {
+    const user_id = result.rows[i].user_id;
+    const checkHashedUser = sha256(user_id + SALT)
+    console.log(hashedId === checkHashedUser)
+    if (hashedId === checkHashedUser) return user_id;
+  }
+  return "";
+}
+
+async function checkSongDuplicates(pool, user_id, checkValue) {
+  const queryText = "SELECT * FROM songs WHERE user_id=$1 AND video_link=$2";
+  const values = [user_id, checkValue];
+  const result = await pool.query(queryText, values);
+  console.log(result)
+  if (result.rows.length !== 0) return true;
+  else return false;
+}
+
+export {checkBlanks, validateEmail, validation, checkSongDuplicates};
